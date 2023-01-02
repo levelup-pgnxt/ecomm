5cdb24f05f19f9717edbb5d15a50535f952675e8
Cairo Ribeiro <cairo.gr@live.com>
2023-01-02 17:46:36 -0300

    Created function to create user account

---
+++ account/test/createUserAccount.test.js
@@ -0,0 +1,16 @@
+import { createUserUseCase, accounts } from '../src/use-case/createUserAccount.js';
+
+
+
+createUserUseCase(`Nome`,`nome1@dominio.com`, 123456);
+createUserUseCase(`Nome`,`nome2@dominio.com`, 123456);
+createUserUseCase(`Nome`,`nome3@dominio.com`, 123456);
+createUserUseCase(`Nome`,`nome4@dominio.com`, 123456);
+createUserUseCase(`Nome`,`nome5@dominio.com`, 123456);
+createUserUseCase(`Nome`,`nome6@dominio.com`, 123456);
+createUserUseCase(`Nome`,`nome7@dominio.com`, 123456);
+
+
+//console.log(accounts.length);
+console.log(accounts);
+// console.log(typeof(createUserUseCase));
