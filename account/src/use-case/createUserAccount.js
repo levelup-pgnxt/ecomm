74af083c4623c875244b2297e74ad4f75c4b9bcf
RenzoOliveira1784 <Renzorib1218@gmail.com>
2023-01-03 18:08:56 -0300

    create user feature implemented

---
+++ account/src/use-case/createUserAccount.js
@@ -0,0 +1,15 @@
+const allAccounts = []
+
+export default function createUserUseCase(name, email, password) {
+    const userObject = {
+        id: allAccounts.length + 1,
+        name: name,
+        email: email,
+        password: password,
+        createdDate: JSON.stringify(new Date()).substring(1, 11)
+    };
+
+    allAccounts.push(userObject);
+    return userObject;
+}
+
---
+++ account/test/createUserAccount.test.js
@@ -0,0 +1,8 @@
+// importa a função criada no createUserAccount.js
+// executa passando os parametros necessarios e 
+// usa o retorno da funcao no console.log
+
+import createUserUseCase from "./../src/use-case/createUserAccount.js";
+
+const retro = createUserUseCase("Josué Lima", "josuelima@email.com", "senhaDoJosue");
+console.log(retro);
