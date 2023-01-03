ce8b3499b99c4852b886f513d6c12432c8a637fd
= <paulo.leite@pagonxt.com>
2023-01-03 11:37:53 -0300

    feat: implementada a Classe UsersCustomer

--- account/src/services/utils.js
+++ account/src/services/utils.js
@@ -4,37 +4,32 @@ class UsersCustomer {
     };
 
     push ({ nome, email, senha }) {
-        const id = this.getId();
-        const date = new Date();
-        const createdDate = Date.parse(date.toISOString('YYYY-MM-DD'));
-        console.log(id, createdDate);
-        const newUser = { id, name: nome, email, password: senha, createdDate }
+        const id = this._getId();
+        const createdDate = new Date().toLocaleDateString('sv');
+        const newUser = {
+            id,
+            name: nome,
+            email,
+            password: senha,
+            createdDate
+        }
         this.usersCustomer.push(newUser);
         return newUser;
     };
     
-    getId () {
-        if (this.usersCustomer.length === 0) {
-            return 1;
-        }
+    _getId () {
+        if (this.usersCustomer.length === 0) return 1;
         const lastIndex = this.usersCustomer.length - 1;
-        return this.usersCustomer[lastIndex].id + 1
+        return this.usersCustomer[lastIndex].id + 1;
+    };
+
+    listAllUsers () {
+        const allUsers = [];
+        this.usersCustomer.forEach((user) => {
+            allUsers.push([user.id, user.name, user.email, user.createdDate]);
+        });
+        return allUsers;
     };
 };
 
-const users = new UsersCustomer();
-
-const data = {
-    nome: "Paulo Leite",
-    email: "phvleite@gmail.com",
-    senha: "123456"
-}
-
-const data2 = {
-    nome: "Paulo Leite",
-    email: "phvleite@gmail.com",
-    senha: "123456"
-}
-
-console.log(users.push(data));
-console.log(users.push(data2));
+export default UsersCustomer;
