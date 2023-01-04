70d3b67a71a2dd602a45f912065e16d9a8e791d0
Cairo Ribeiro <cairo.gr@live.com>
2023-01-04 13:00:32 -0300

    Created Dockerfile and updated docker-compose

--- account/src/use-case/searchUserAccountByUF.js
+++ account/src/use-case/searchUserAccountByUF.js
@@ -11,17 +11,4 @@ function searchUserAccountByUFUseCase(UFFind) {
   }
 }
 
-createUserUseCase(`Cairo`,`nome1@dominio.com`, 123456);
-createUserUseCase(`Carol`,`nome2@dominio.com`, 123456);
-createUserUseCase(`Karina`,`nome3@dominio.com`, 123456);
-createUserUseCase(`Gabriel`,`nome4@dominio.com`, 123456);
-
-
-createAdditionalDataUseCase(`nome1@dominio.com`, "rua", 210, "perto", "aqui", 39000, "Montes Claros", "MG");
-createAdditionalDataUseCase(`nome3@dominio.com`, "rua", 230, "perto", "aqui", 39999, "Ouro Preto", "MG");
-
-
-
-console.log(searchUserAccountByUFUseCase("MG"));
-
 export { searchUserAccountByUFUseCase };
