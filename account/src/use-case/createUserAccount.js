d4a1c080e6fce8aa22cd00b3ba25c0e074df7e72
ARAUJO ANA REBECA MARQUES <n919431@gruposantander.com>
2023-01-04 12:39:06 -0300

    Implementando funções createUserAccount e createUserAccount.test

---
+++ account/src/use-case/createUserAccount.js
@@ -0,0 +1,15 @@
+const accounts = [];
+
+export default function createUserUseCase (name, email, password){
+    let currentDate = new Date().toJSON().slice(0,10);
+
+    const user = {
+        id: accounts.length +1,
+        nome: name,
+        email: email,
+        password: password,
+        createdDate: currentDate
+    }
+    accounts.push(user);
+    return accounts[accounts.length-1];
+}
