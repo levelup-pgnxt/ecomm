a2f95a5675ceed4c7dcd37d4a0140d13806fa5ec
= <paulo.leite@pagonxt.com>
2023-01-04 11:04:49 -0300

    feat: implementada a função searchUserAccountByEmail()

---
+++ account/src/use-case/searchUserAccountByEmail.js
@@ -0,0 +1,13 @@
+import { usersCustomer, validateEmail } from '../services/utils.js';
+
+const seachrUserAccountByEmail = (data) => {
+    const { value, error } = validateEmail(data);
+    if (error) return error.message;
+
+    const userByEmail = usersCustomer.getUserByEmail(value);
+
+    if (userByEmail === undefined) return "Usuário não localizado";
+    return userByEmail;
+};
+
+export default seachrUserAccountByEmail;
