0ac18526515e46ba7557813f4c41da23b2a47bc5
Gabriella Duarte <gabriella.duarte@pagonxt.com>
2023-01-05 16:07:49 -0300

    feat: add funcao createUserUseCase que cria um novo usuario e o adiciona em um array

---
+++ account/src/use-case/createUserAccount.js
@@ -0,0 +1,15 @@
+export const accounts = [];
+
+export function createUserUseCase(name, email, password) {
+  const newUser = {
+    id: accounts.length + 1,
+    name,
+    email,
+    password,
+    createdDate: new Date().toISOString().substring(0, 10),
+  };
+
+  accounts.push(newUser);
+
+  return newUser;
+}
