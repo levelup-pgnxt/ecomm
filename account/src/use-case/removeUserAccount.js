57b0c050c4907a7f72db50af777d05b9a1f15b18
Gabriella Duarte <gabriella.duarte@pagonxt.com>
2023-01-06 17:11:44 -0300

    feat: add funcao removeUserUseCase que remove um usuario pelo email passado

---
+++ account/src/use-case/removeUserAccount.js
@@ -0,0 +1,14 @@
+import { accounts } from "./createUserAccount.js";
+import { searchUserAccountByEmailCase } from "./searchUserAccountByEmail.js";
+
+export function removeUserUseCase(email) {
+  const user = searchUserAccountByEmailCase(email);
+
+  if (typeof user === "object") {
+    accounts.splice(accounts.indexOf(user), 1);
+
+    return true;
+  }
+
+  return false;
+}
