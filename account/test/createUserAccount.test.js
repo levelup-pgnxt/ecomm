5a76bef23194b61618b8204a8ea46d011bab163f
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2023-01-06 17:02:04 -0300

    Criando a conta de usuário

---
+++ account/test/createUserAccount.test.js
@@ -0,0 +1,9 @@
+import { createUserUseCase } from '../src/use-case/createUserAccount.js';
+
+const josue = createUserUseCase('Josué Lima', 'josuelima@gmail.com', 'senhaDoJosue');
+
+const alexandre = createUserUseCase('Alexandre Aquiles', 'alexandre@gmail.com', 'senhaDoAlexandre');
+
+
+console.log(josue);
+console.log(alexandre);
