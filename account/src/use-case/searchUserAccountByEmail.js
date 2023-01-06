181a361cd9c38d5b39054aeb1c3eb3bcfb0de475
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2023-01-06 17:57:21 -0300

    Revert "Semana2 js arrays e objetos"

--- account/src/use-case/searchUserAccountByEmail.js
+++
@@ -1,7 +0,0 @@
-import { getAccounts } from './createUserAccount.js'
-
-export function searchUserAccountByEmailUseCase(email) {
-
-   return getAccounts().find(account => account.email == email);
-
-}
--- account/test/createUserAccount.test.js
+++
@@ -1,9 +0,0 @@
-import { createUserUseCase } from '../src/use-case/createUserAccount.js';
-
-const josue = createUserUseCase('Josué Lima', 'josuelima@gmail.com', 'senhaDoJosue');
-
-const alexandre = createUserUseCase('Alexandre Aquiles', 'alexandre@gmail.com', 'senhaDoAlexandre');
-
-
-// console.log(josue);
-// console.log(alexandre);
