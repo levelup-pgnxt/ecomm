36334a9501ce2296accd247ef9da8439517b9693
Gabriella Duarte <gabriella.duarte@pagonxt.com>
2023-01-20 15:49:23 -0300

    refactor: altera importação de accounts

--- account/src/use-case/removeUserAccount.js
+++ account/src/use-case/removeUserAccount.js
@@ -1,10 +1,10 @@
-import { accounts } from "./createUserAccount.js";
-import { searchUserAccountByEmailCase } from "./searchUserAccountByEmail.js";
+import { accounts } from './mockAccounts.js';
+import { searchUserAccountByEmailCase } from './searchUserAccountByEmail.js';
 
 export function removeUserUseCase(email) {
   const user = searchUserAccountByEmailCase(email);
 
-  if (typeof user === "object") {
+  if (typeof user === 'object') {
     accounts.splice(accounts.indexOf(user), 1);
 
     return true;
--- account/src/use-case/searchUserAccountByEmail.js
+++ account/src/use-case/searchUserAccountByEmail.js
@@ -1,9 +1,9 @@
-import { accounts } from "./createUserAccount.js";
+import { accounts } from './mockAccounts.js';
 
 export function searchUserAccountByEmailCase(email) {
   const user = accounts.find((account) => account.email === email);
   if (!user) {
-    return `Esse usuário não foi encontrado`;
+    return 'Esse usuário não foi encontrado';
   }
 
   return user;
