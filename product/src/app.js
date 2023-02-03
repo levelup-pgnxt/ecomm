517568f77882f65131227df35178ec4a8a7eab0f
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-02-03 08:00:45 -0300

    <setup> Config app.js and server.js

--- product/src/app.js
+++ product/src/app.js
@@ -1,1 +1,14 @@
-const express = require('express')
+require('express-async-errors');
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use('/api/categories');
+app.use('/api/admin/categories');
+
+module.exports = app;
---
+++ product/src/server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+PORT = 3001
+
+app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
