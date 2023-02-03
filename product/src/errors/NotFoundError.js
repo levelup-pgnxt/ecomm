1daad400a05297a5b683194309104fa393663655
= <paulo.leite@pagonxt.com>
2023-02-03 20:05:41 -0300

    feat: criada a classe NotFoundError para tratamentos de erros

---
+++ product/src/errors/NotFoundError.js
@@ -0,0 +1,19 @@
+const MESSAGE_ERROR = {
+    'Categoria já cadastrada!': 409,
+    'Produto já cadastrado!': 409,
+};
+
+class NotFoundError extends Error {
+    constructor(message) {
+      super(message);
+      const keysMessageError = Object.keys(MESSAGE_ERROR);
+      this.name = 'NotFoundError';
+      if (keysMessageError.includes(message)) {
+        this.code = MESSAGE_ERROR[message];
+      } else {
+        this.code = 404;
+      }
+    }
+  }
+  
+  export default NotFoundError;
