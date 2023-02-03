64dab00295b1923527ad16a4bff757389ce2d22d
= <paulo.leite@pagonxt.com>
2023-02-03 13:16:04 -0300

    fix: limpeza console.log

--- product/src/services/validator.js
+++ product/src/services/validator.js
@@ -2,7 +2,6 @@ const runSchema = (schema) => (data) => {
     const { error, value } = schema.validate(data);
   
     if (error) {
-      // console.log(error);
       let msg = error.details[0].message;
       if (msg.substr(1, 1) === '[') {
         msg = msg.replace(msg.substr(1, 4), '');
