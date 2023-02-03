607c12e0f5974fbc8efc3af8db78804fd42f56fa
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-02-03 10:13:14 -0300

    <feat> Create categories done

--- product/src/controllers/categories.controller.js
+++ product/src/controllers/categories.controller.js
@@ -1,15 +1,22 @@
 const CategoriesModel = require('../models/categories.model');
-const HTTPStatus = require('../../../helpers/HTTP.status')
+const HTTPStatus = require('../../../helpers/HTTP.status');
+const validations = require('../validations/categories.validations');
 
 const findAll = async (_req, res) => {
   const response = await CategoriesModel.find();
-  res.status(HTTPStatus.OK).json(response);
+  return res.status(HTTPStatus.OK).json(response);
 }
 
 const create = async (req, res) => {
   const payload = req.body;
+  const valid = validations.create(payload);
+  if (valid !== null) return res.status(valid.status).send(valid.message);
+
+  const response = await CategoriesModel.create(payload);
+  return res.status(HTTPStatus).json(response);
 }
 
 module.exports = {
   findAll,
+  create,
 }
