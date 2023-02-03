7af75e1a65bfd93f421d8eaddfbc570526f6bd5e
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-02-03 19:32:33 -0300

    <fix> Fixed bugs and adjustments made to the seeders

---
+++ helpers/HTTP.status.js
@@ -0,0 +1,12 @@
+const HTTPStatus = {
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  UN_ENTITY: 422,
+  INTERNAL: 500,
+}
+
+module.exports = HTTPStatus;
