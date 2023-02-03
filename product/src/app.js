ce82bf9d494267001d287c9a8699ba636a473bdd
= <paulo.leite@pagonxt.com>
2023-02-02 23:58:46 -0300

    feat: inclusão do express-async-errors e do middleware de erros

--- product/src/app.js
+++ product/src/app.js
@@ -1,6 +1,8 @@
+import 'express-async-errors';
 import express from "express";
 import db from './config/dbConnect.js';
 import routes from './routes/index.js'
+import errorHandlerMiddleware from './services/erroHandlerMiddleware.js';
 
 db.on('error', console.log.bind(console, 'Erro de conexão!'));
 
@@ -13,5 +15,7 @@ const app = express();
 app.use(express.json());
 
 routes(app)
+
+app.use(errorHandlerMiddleware);
 
 export default app;
