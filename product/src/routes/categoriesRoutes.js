dd2ea2af2627d576755fc6c5e27c0648b314eeab
= <paulo.leite@pagonxt.com>
2023-02-04 06:26:23 -0300

    fix: correção das rotas

--- product/src/routes/categoriesRoutes.js
+++ product/src/routes/categoriesRoutes.js
@@ -4,15 +4,16 @@ import categoriesController from '../controllers/categoriesController.js';
 const router = express.Router();
 
 const path = '/categories';
-const pathId = '/categories/:id'
+const pathId = '/categories/:id';
+const pathAdmin = '/admin/categories/:id'
 
 router
     .get(path, categoriesController.getAllCategories)
     .get(`${path}/search`, categoriesController.getCategoryByName)
     .get(pathId, categoriesController.getCategoryById)
-    .post(path, categoriesController.createCategory)
-    .put(pathId, categoriesController.updateCategory)
-    .patch(pathId, categoriesController.activateDeactivateCategory)
-    .delete(pathId, categoriesController.deleteCategoryById)
+    .post(`/admin${path}`, categoriesController.createCategory)
+    .put(pathAdmin , categoriesController.updateCategory)
+    .patch(pathAdmin, categoriesController.activateDeactivateCategory)
+    .delete(pathAdmin, categoriesController.deleteCategoryById)
 
 export default router;
--- product/src/routes/productsRoutes.js
+++ product/src/routes/productsRoutes.js
@@ -5,14 +5,15 @@ const router = express.Router();
 
 const path = '/products';
 const pathId = '/products/:id'
+const pathAdmin = '/admin/products/:id'
 
 router
     .get(path, productsController.getAllProducts)
     .get(`${path}/search`, productsController.getProductByName)
     .get(pathId, productsController.getProductById)
-    .post(path, productsController.createProduct)
-    .put(pathId, productsController.updateProduct)
-    .patch(pathId, productsController.activateDeactivateProduct)
-    .delete(pathId, productsController.deleteProductById)
+    .post(`/admin${path}`, productsController.createProduct)
+    .put(pathAdmin, productsController.updateProduct)
+    .patch(pathAdmin, productsController.activateDeactivateProduct)
+    .delete(pathAdmin, productsController.deleteProductById)
 
 export default router;
