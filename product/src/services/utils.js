67806f82e24bcbe11efcb5a901f029bcc8b75732
= <paulo.leite@pagonxt.com>
2023-02-04 13:37:53 -0300

    feat: implatação das validações do cadastro de um novo produto

--- product/src/services/utils.js
+++ product/src/services/utils.js
@@ -6,7 +6,7 @@ const limit = 3;
 
 const validates = {
     paramsCategory: runSchema(Joi.object().keys({
-        nome: Joi.string().min(limit).empty().required().pattern(/^[A-ZÇÂÃÕÉÁÓ]+/).messages({
+        nome: Joi.string().min(limit).empty().required().pattern(/^[A-Z][A-ZÇÂÃÕÉÁÓ\s]+$/).messages({
             'string.base': 'O campo "nome" deve ser do tipo texto!',
             'string.empty': 'O campo "nome" não deve ser vazio!',
             'string.min': `'O campo "nome" deve ter no mínimo ${limit} caracteres!'`,
@@ -17,6 +17,48 @@ const validates = {
 
     paramsID: (id) => mongoose.Types.ObjectId.isValid(id),
 
+    paramsProduct: runSchema(Joi.object().keys({
+        nome: Joi.string().min(limit).empty().required().pattern(/^[A-Z].+/).messages({
+            'string.base': 'O campo "nome" deve ser do tipo texto!',
+            'string.empty': 'O campo "nome" não deve ser vazio!',
+            'string.min': `'O campo "nome" deve ter no mínimo ${limit} caracteres!'`,
+            'string.pattern.base': 'O campo "nome" deve iniciar por uma letra maiúscula!',
+            'any.required': 'O campo "nome" é obrigatório!'
+        }),
+        descricao: Joi.string().min(limit).empty().required().pattern(/^[A-Z].+/).messages({
+            'string.base': 'O campo "descrição" deve ser do tipo texto!',
+            'string.empty': 'O campo "descrição" não deve ser vazio!',
+            'string.min': `'O campo "descrição" deve ter no mínimo ${limit} caracteres!'`,
+            'string.pattern.base': 'O campo "descrição" deve iniciar por uma letra maiúscula!',
+            'any.required': 'O campo "descrição" é obrigatório!'
+        }),
+        slug: Joi.string().min(limit).empty().required().pattern(/^[A-Za-z0-9-\S]+$/).messages({
+            'string.base': 'O campo "slug" deve ser do tipo texto!',
+            'string.empty': 'O campo "slug" não deve ser vazio!',
+            'string.min': `'O campo "slug" deve ter no mínimo ${limit} caracteres!'`,
+            'string.pattern.base': 'O campo "slug" deve conter apenas letras(maiúsculas ou minúsculas), números ou hífens!',
+            'any.required': 'O campo "slug" é obrigatório!'
+        }),
+        precoUnitario: Joi.number().precision(2).greater(0).required().messages({
+            'number.base': 'O campo "preço unitário" deve ser do tipo numérico!',
+            'number.greater': 'O campo "preço unitário" deve ser maior que zero!',
+            'any.required': 'O campo "preço unitário" é obrigatório!'
+        }),
+        estoque: Joi.number().greater(0).less(10000).required().messages({
+            'number.base': 'O campo "estoque" deve ser do tipo numérico!',
+            'number.greater': 'O campo "estoque" deve ser maior que zero!',
+            'number.less': 'O campo "estoque" deve ser menor que 10.000!',
+            'any.required': 'O campo "estoque" é obrigatório!'
+        }),
+        desconto: Joi.number().precision(2).messages({
+            'number.base': 'O campo "desconto" deve ser do tipo numérico!',
+        }),
+        categoria: Joi.string().empty().required().messages({
+            'string.base': 'O campo "categoria" deve ser do tipo texto!',
+            'string.empty': 'O campo "categoria" não deve ser vazio!',
+            'any.required': 'O campo "categoria" é obrigatório!'
+        }),
+    })),
 };
 
 
