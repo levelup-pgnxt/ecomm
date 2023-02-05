a36178b7b1549141f98ad6642bd5511793a8d54f
= <paulo.leite@pagonxt.com>
2023-02-05 15:32:33 -0300

    realizando testes

--- product/src/main.js
+++ product/src/main.js
@@ -1,1 +1,14 @@
-console.log('iniciando product');
+// console.log('iniciando product');
+
+const regexString = new RegExp(/[a-z]/i);
+const regexNumber = new RegExp(/\d/);
+const regexSpecial = new RegExp(/[$%#@\.+-]/);
+const senha = '12a3456';
+
+const hasString = regexString.test(senha);
+const hasNumber = regexNumber.test(senha);
+const hasSpecial = regexSpecial.test(senha);
+
+const validateSenha = hasString && hasNumber && hasSpecial;
+
+console.log();
