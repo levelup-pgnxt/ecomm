43acfa47d198f98ab41657d534a17ea15219dfdb
= <paulo.leite@pagonxt.com>
2023-02-05 16:37:44 -0300

    fix: arquivo main criado em account

---
+++ account/src/main.js
@@ -0,0 +1,18 @@
+console.log('iniciando account');
+
+const regexString = new RegExp(/[a-z]/i);
+const regexNumber = new RegExp(/\d/);
+const regexSpecial = new RegExp(/[$%#&*!?\.+-]/);
+const senha = 'Pagonext23%23';
+
+const hasString = regexString.test(senha);
+const hasNumber = regexNumber.test(senha);
+const hasSpecial = regexSpecial.test(senha);
+
+const validateSenha = hasString && hasNumber && hasSpecial;
+
+console.log("hasString ", hasString);
+console.log("hasNumber ", hasNumber);
+console.log("hasSpecial ", hasSpecial);
+console.log("ValidateSenha ", validateSenha);
+
