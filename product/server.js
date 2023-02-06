7633f0880e4e734b7e89722bf488ce260659c403
Matheus Leme <matheuslemelhp@gmail.com>
2023-02-06 13:50:48 -0300

    Ajuste na porta do servidor da API de produtos

--- product/server.js
+++ product/server.js
@@ -1,5 +1,5 @@
 import app from "./src/app.js";
 
-const port = process.env.port || 3000;
+const port = process.env.port || 3001;
 
 app.listen(port, () => console.log(`Servidor escutando em http://localhost:${port}`));
