96408d9e2fdc09298ff99063acb9801add4f1dac
= <paulo.leite@pagonxt.com>
2023-02-05 22:20:48 -0300

    feat: criado o arquivo NotFoundError para gerenciamentos de erros

---
+++ account/src/errors/NotFoundError.js
@@ -0,0 +1,23 @@
+const MESSAGE_ERROR = {
+    'Categoria já cadastrada!': 409,
+    'Produto já cadastrado!': 409,
+    'Valor mínimo maior que valor máximo. Operação não permitida!': 405,
+    'Senha inválida!': 400,
+    'Estado da Federação inválido!': 400,
+    'CPF inválido!': 400,
+};
+
+class NotFoundError extends Error {
+    constructor(message) {
+      super(message);
+      const keysMessageError = Object.keys(MESSAGE_ERROR);
+      this.name = 'NotFoundError';
+      if (keysMessageError.includes(message)) {
+        this.code = MESSAGE_ERROR[message];
+      } else {
+        this.code = 404;
+      }
+    }
+}
+  
+export default NotFoundError;
