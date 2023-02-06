24c34d971ce7c0351b99c38863fb89e00752f3c2
Matheus Leme <matheuslemelhp@gmail.com>
2023-02-06 13:55:30 -0300

    Ajustando local das pastas da API de Produtos

--- product/src/app.js
+++ product/src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import db from '../config/dbConnect.js';
-import routes from '../routes/index.js'
+import db from './config/dbConnect.js';
+import routes from './routes/index.js';
 
 db.on('error', console.log.bind(console, 'Erro de conexão com o banco.'));
 db.once('open', () => console.log('Conexão com o banco realizada com sucesso.'));
