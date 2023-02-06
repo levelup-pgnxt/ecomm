2c02e8469ae8f2feb237b19c227b1d83d8eb0ff4
= <paulo.leite@pagonxt.com>
2023-02-06 06:15:30 -0300

    fix: ajustes das rotas

--- account/src/routes/usersRoutes.js
+++ account/src/routes/usersRoutes.js
@@ -8,12 +8,11 @@ const pathId = '/users/:id';
 const pathAdmin = '/admin/users/:id'
 
 router
-    .get(path, usersController.getAllUsers)
-    .get(`${path}/search`, usersController.getUserByName)
+    .get(`/admin${path}`, usersController.getAllUsers)
+    .get(`/admin${path}/search`, usersController.getUserByName)
     .get(pathId, usersController.getUserById)
     .post(`/admin${path}`, usersController.createUser)
     .put(pathAdmin , usersController.updateUser)
-    .patch(pathAdmin, usersController.activateDeactivateUser)
     .delete(pathAdmin, usersController.deleteUserById)
 
 export default router;
