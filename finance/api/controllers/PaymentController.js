f48136f538ecf9defc2b2094ca9947c3c8f70441
Ana Rebeca <anarebeca.ar@gmail.com>
2023-02-15 15:53:46 -0300

    Endpoints de confirmação e cancelamento

--- finance/api/controllers/PaymentController.js
+++ finance/api/controllers/PaymentController.js
@@ -41,6 +41,46 @@ class PaymentController{
             return res.status(500).json(error.message);
         }
     }
+
+    static async cancelPayment(req, res){
+        const { id } = req.params;
+        const cancel = { "status":"CANCELADO" };
+        try {
+            await database.Payments.update(cancel, {
+                where:{
+                    id:Number(id)
+                }
+            });
+            const cancelledPayment = await database.Payments.findOne({
+                where:{
+                    id:Number(id)
+                }
+            });
+            return res.status(201).json(cancelledPayment);
+        }  catch (error){
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async confirmPayment(req, res){
+        const { id } = req.params;
+        const confirm = { "status":"CONFIRMADO" };
+        try {
+            await database.Payments.update(confirm, {
+                where:{
+                    id:Number(id)
+                }
+            });
+            const confirmedPayment = await database.Payments.findOne({
+                where:{
+                    id:Number(id)
+                }
+            });
+            return res.status(201).json(confirmedPayment);
+        }  catch (error){
+            return res.status(500).json(error.message);
+        }
+    }
 }
 
 module.exports = PaymentController; 
--- finance/api/routes/paymentsRoute.js
+++ finance/api/routes/paymentsRoute.js
@@ -6,6 +6,9 @@ const router = express.Router();
 router.get("/payments/:id", PaymentController.getPaymentById)
 router.get("/payments", PaymentController.getPayments)
 router.post("/payments", PaymentController.createPayment)
+router.put("/payments/cancel/:id", PaymentController.cancelPayment)
+router.put("/payments/confirm/:id", PaymentController.confirmPayment)
+
 //   .put("/payment/:id", PaymentController.updatePayment)
 //   .put("/payment/:id", PaymentController.activatePayment)
 //   .delete("/payment/:id", PaymentController.deletePayment)
