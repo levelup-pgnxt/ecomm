1211e8acedae0a77d4876c9e20b4ca32cddb0985
= <paulo.leite@pagonxt.com>
2023-02-15 13:41:13 -0300

    fix: correção das rotas product

--- product/src/routes/productsRoutes.js
+++ product/src/routes/productsRoutes.js
@@ -4,19 +4,23 @@ import productsController from '../controllers/productsController.js';
 const router = express.Router();
 
 const path = '/products';
-const pathSearch = '/products/search';
+const pathSearch = `${path}/search`;
+const pathSearchByValue = `${pathSearch}-by-value`;
+const pathSearchByStock = `${pathSearch}-by-stock`;
+const pathSearchByCategoryId = `${pathSearch}-by-category/:id`;
 const pathId = '/products/:id'
-const pathAdmin = '/admin/products/:id'
+const pathAdminProducts = `/admin${path}`
+const pathAdminProductsByID = `/admin${pathId}`
 
 router
     .get(path, productsController.getAllProducts)
     .get(pathSearch, productsController.getProductByName)
-    .get(`${pathSearch}-by-value`, productsController.getProductsByValue)
-    .get(`${pathSearch}-by-stock`, productsController.getProductsByStock)
-    .get(`${pathSearch}-by-category/:id`, productsController.getProductsByCategoryId)
+    .get(pathSearchByValue, productsController.getProductsByValue)
+    .get(pathSearchByStock, productsController.getProductsByStock)
+    .get(pathSearchByCategoryId, productsController.getProductsByCategoryId)
     .get(pathId, productsController.getProductById)
-    .post(`/admin${path}`, productsController.createProduct)
-    .put(pathAdmin, productsController.updateProduct)
-    .delete(pathAdmin, productsController.deleteProductById)
+    .post(pathAdminProducts, productsController.createProduct)
+    .put(pathAdminProductsByID, productsController.updateProduct)
+    .delete(pathAdminProductsByID, productsController.deleteProductById)
 
 export default router;
