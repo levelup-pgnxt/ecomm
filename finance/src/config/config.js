54292365b48ab0e228997a40ac118291f3cf27a9
= <paulo.leite@pagonxt.com>
2023-02-15 13:28:57 -0300

    fix: ajuste das variáveis

--- finance/src/config/config.js
+++ finance/src/config/config.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 
 const config = {
-  username: process.env.MYSQL_USER,
-  password: null,
-  database: process.env.MYSQL_DATABASE,
-  host: process.env.MYSQL_HOST,
+  HOST: process.env.MYSQL_HOST,
+  USER: process.env.MYSQL_USER,
+  PASSWORD: process.env.MYSQL_PASSWORD,
+  ROOT_PASSWORD: process.env.MYSQL_ROOT_PASSWORD,
+  MYSQL: process.env.MYSQL_NAME,
+  port: process.env.MYSQL_PORT,
   dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 };
 
 module.exports = {
