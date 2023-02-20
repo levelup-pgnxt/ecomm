1dcd69103ae5314c20b4c8a87e5a927106d7b640
= <paulo.leite@pagonxt.com>
2023-02-20 11:56:49 -0300

    fix: verificação para corrigir a conexão

--- finance/src/config/config.js
+++ finance/src/config/config.js
@@ -1,19 +1,25 @@
 require('dotenv').config();
 
 const config = {
-  host: process.env.MYSQL_HOST,
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  root_password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
+  username: 'root',
+  password: null,
+  database: 'ecomm-finance',
+  host: '127.0.0.1',
   dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+  operatorAliases: false
+  // host: process.env.MYSQL_HOST,
+  // username: process.env.MYSQL_USER,
+  // password: process.env.MYSQL_PASSWORD,
+  // root_password: process.env.MYSQL_ROOT_PASSWORD,
+  // database: process.env.MYSQL_DATABASE,
+  // port: process.env.MYSQL_PORT,
+  // dialect: 'mysql',
+  // pool: {
+  //   max: 5,
+  //   min: 0,
+  //   acquire: 30000,
+  //   idle: 10000
+  // }
 };
 
 module.exports = {
