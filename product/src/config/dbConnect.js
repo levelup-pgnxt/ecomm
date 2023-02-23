283e6034e8e8dde5aadc6deeebfb8815002e00b3
= <paulo.leite@pagonxt.com>
2023-02-22 23:03:56 -0300

    fix: para verificar, testes não rodam quando o banco está setado para o caminho no container, apenas com localhost.

--- product/src/config/dbConnect.js
+++ product/src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
-// mongoose.connect('mongodb://admin:secret@127.0.0.1:27017/ecomm?authSource=admin');
-mongoose.connect('mongodb://admin:secret@ecomm-mongodb:27017/ecomm?authSource=admin');
+mongoose.connect('mongodb://admin:secret@127.0.0.1:27017/ecomm?authSource=admin');
+// mongoose.connect('mongodb://admin:secret@ecomm-mongodb:27017/ecomm?authSource=admin');
 
 const db = mongoose.connection;
 
