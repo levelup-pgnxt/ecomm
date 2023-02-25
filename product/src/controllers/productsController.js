93474e53499a407d23abf3fe488c71854c0a347a
= <paulo.leite@pagonxt.com>
2023-02-24 22:47:58 -0300

    fix: melhor verbalização para o retorno do erro do id inválido da categoria

--- product/src/controllers/productsController.js
+++ product/src/controllers/productsController.js
@@ -89,7 +89,7 @@ class ProductController {
             throw new NotFoundError(message);
         };
         const isValideIdCategory = validates.paramsID(categoria);
-        if (!isValideIdCategory) return res.status(400).send({ message: 'ID inválido!' });
+        if (!isValideIdCategory) return res.status(400).send({ message: 'ID categoria inválido!' });
 
         const isExistCategory = await CategoryService.checkIsExistsCategoryById(categoria);
         if (!isExistCategory) return res.status(400).send({ message: 'Categoria não localizada!' });
