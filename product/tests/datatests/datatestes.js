4ff640839672a270e0307836a2ab214da94ee484
= <paulo.leite@pagonxt.com>
2023-02-24 22:50:31 -0300

    feat: criação de arquivo para ser base de dados para os testes

---
+++ product/tests/datatests/datatestes.js
@@ -0,0 +1,36 @@
+const DATATEST = [
+    {
+        nome: "Multiprocessador KitchenAid KJA03B 240W empire red 127V",
+        descricao: "Este multiprocessador permitirá que você otimize o espaço e o tempo de preparação de seus pratos favoritos.",
+        slug: "multiprocessador",
+        precoUnitario: 577,
+        estoque: 27,
+        categoria: "63dd8be23138b0c3320e26fe"
+    },
+    {
+        nome: "Multiprocessador KitchenAid KJA09B 250W empire red 110V",
+        descricao: "Você poderá resolver seus almoços e jantares em poucos passos e sem limitações. Amassa a mistura de seus biscoitos ou pães caseiros e vive a aventura de ter uma padaria em casa.",
+        slug: "multiprocessador",
+        precoUnitario: 1136.72,
+        estoque: 2,
+        categoria: "63dd8be23138b0c3320e26fe"
+    },
+    {
+        nome: "Multiprocessador KitchenAid KJA09B 250W empire red 110V",
+        descricao: "Você poderá resolver seus almoços e jantares em poucos passos e sem limitações. Amassa a mistura de seus biscoitos ou pães caseiros e vive a aventura de ter uma padaria em casa.",
+        slug: "multiprocessador",
+        precoUnitario: 1136.72,
+        estoque: 2,
+        categoria: "0123456789"
+    },
+    {
+        nome: "Mini Processador Black Decker Tritura Processa Tigela 2l",
+        descricao: "Quando se trata de fazer seus pratos favoritos, todas as suas necessidades serão facilmente cobertas com este multiprocessador Black+Decker. Descobra uma nova maneira de cozinhar e surpreenda a sua família com as preparações mais ricas. Este aparelho transformará os alimentos no menor tempo possível e se tornará seu ajudante obrigatório na cozinha.",
+        slug: "multiprocessador",
+        precoUnitario: 281.30,
+        estoque: 2,
+        categoria: "66dd8be23138b0c3320e26fe"
+    },
+];
+
+export default DATATEST;
