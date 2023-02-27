1c20f435358bfc4897acb3389d593ed062c49c84
= <paulo.leite@pagonxt.com>
2023-02-27 15:03:59 -0300

    test: criação de dados para testes do campo nome do produto

--- product/tests/datatests/datatestes.js
+++ product/tests/datatests/datatestes.js
@@ -31,6 +31,65 @@ const DATATEST = [
         estoque: 2,
         categoria: "66dd8be23138b0c3320e26fe"
     },
+    {
+        nome: "Mini Processador Black Decker Tritura Processa Tigela 2l",
+        descricao: "Quando se trata de fazer seus pratos favoritos, todas as suas necessidades serão facilmente cobertas com este multiprocessador Black+Decker. Descobra uma nova maneira de cozinhar e surpreenda a sua família com as preparações mais ricas. Este aparelho transformará os alimentos no menor tempo possível e se tornará seu ajudante obrigatório na cozinha.",
+        slug: "multiprocessador",
+        precoUnitario: 281.30,
+        estoque: 2,
+        categoria: "63bc6ed7b0e9faa4cdfa0ef2"
+    },
+    {
+        nome: 1234567890,
+        descricao: "Quando se trata de fazer seus pratos favoritos, todas as suas necessidades serão facilmente cobertas com este multiprocessador Black+Decker. Descobra uma nova maneira de cozinhar e surpreenda a sua família com as preparações mais ricas. Este aparelho transformará os alimentos no menor tempo possível e se tornará seu ajudante obrigatório na cozinha.",
+        slug: "multiprocessador",
+        precoUnitario: 281.30,
+        estoque: 2,
+        categoria: "63bc6ed7b0e9faa4cdfa0ef2"
+    },
+    {
+        nome: '',
+        descricao: "Quando se trata de fazer seus pratos favoritos, todas as suas necessidades serão facilmente cobertas com este multiprocessador Black+Decker. Descobra uma nova maneira de cozinhar e surpreenda a sua família com as preparações mais ricas. Este aparelho transformará os alimentos no menor tempo possível e se tornará seu ajudante obrigatório na cozinha.",
+        slug: "multiprocessador",
+        precoUnitario: 281.30,
+        estoque: 2,
+        categoria: "63bc6ed7b0e9faa4cdfa0ef2"
+    },
+    {
+        nome: 'Ab',
+        descricao: "Quando se trata de fazer seus pratos favoritos, todas as suas necessidades serão facilmente cobertas com este multiprocessador Black+Decker. Descobra uma nova maneira de cozinhar e surpreenda a sua família com as preparações mais ricas. Este aparelho transformará os alimentos no menor tempo possível e se tornará seu ajudante obrigatório na cozinha.",
+        slug: "multiprocessador",
+        precoUnitario: 281.30,
+        estoque: 2,
+        categoria: "63bc6ed7b0e9faa4cdfa0ef2"
+    },
+    {
+        nome: "mini Processador Black Decker Tritura Processa Tigela 2l",
+        descricao: "Quando se trata de fazer seus pratos favoritos, todas as suas necessidades serão facilmente cobertas com este multiprocessador Black+Decker. Descobra uma nova maneira de cozinhar e surpreenda a sua família com as preparações mais ricas. Este aparelho transformará os alimentos no menor tempo possível e se tornará seu ajudante obrigatório na cozinha.",
+        slug: "multiprocessador",
+        precoUnitario: 281.30,
+        estoque: 2,
+        categoria: "63bc6ed7b0e9faa4cdfa0ef2"
+    },
+    {
+        descricao: "Quando se trata de fazer seus pratos favoritos, todas as suas necessidades serão facilmente cobertas com este multiprocessador Black+Decker. Descobra uma nova maneira de cozinhar e surpreenda a sua família com as preparações mais ricas. Este aparelho transformará os alimentos no menor tempo possível e se tornará seu ajudante obrigatório na cozinha.",
+        slug: "multiprocessador",
+        precoUnitario: 281.30,
+        estoque: 2,
+        categoria: "63bc6ed7b0e9faa4cdfa0ef2"
+    },
 ];
 
+/*
+    *0 - Novo produto
+    *1 - Atualização produto
+    *2 - Código categoria inválido
+    *3 - Código categoria inexistente
+    *4 - Código categoria inativa
+    *5 - Campo nome tipo texto
+    *6 - Campo nome vazio
+    *7 - Campo nome deve ter no mínmo 3 caracteres
+    *8 - Campo nome deve iniciar por uma letra maiúscula
+    *9 - Campo nome é obrigatório
+*/
 export default DATATEST;
