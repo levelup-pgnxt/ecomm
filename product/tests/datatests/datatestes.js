32b051abd291d611a3dfd703eafbb24a58ed0a06
= <paulo.leite@pagonxt.com>
2023-02-27 18:43:20 -0300

    test: criação de dados para testes do campo slug

--- product/tests/datatests/datatestes.js
+++ product/tests/datatests/datatestes.js
@@ -74,22 +74,110 @@ const DATATEST = [
     {
         descricao: "Quando se trata de fazer seus pratos favoritos, todas as suas necessidades serão facilmente cobertas com este multiprocessador Black+Decker. Descobra uma nova maneira de cozinhar e surpreenda a sua família com as preparações mais ricas. Este aparelho transformará os alimentos no menor tempo possível e se tornará seu ajudante obrigatório na cozinha.",
         slug: "multiprocessador",
+        precoUnitario: 577,
+        estoque: 27,
+        categoria: "63dd8be23138b0c3320e26fe"
+    },
+    {
+        nome: "Multiprocessador KitchenAid KJA03B 240W empire red 127V",
+        descricao: 1234567890,
+        slug: "multiprocessador",
         precoUnitario: 281.30,
         estoque: 2,
         categoria: "63bc6ed7b0e9faa4cdfa0ef2"
     },
+    {
+        nome: "Multiprocessador KitchenAid KJA03B 240W empire red 127V",
+        descricao: "",
+        slug: "multiprocessador",
+        precoUnitario: 577,
+        estoque: 27,
+        categoria: "63dd8be23138b0c3320e26fe"
+    },
+    {
+        nome: "Multiprocessador KitchenAid KJA03B 240W empire red 127V",
+        descricao: "Qu",
+        slug: "multiprocessador",
+        precoUnitario: 577,
+        estoque: 27,
+        categoria: "63dd8be23138b0c3320e26fe"
+    },
+    {
+        nome: "Multiprocessador KitchenAid KJA03B 240W empire red 127V",
+        descricao: "quando se trata de fazer seus pratos favoritos, todas as suas necessidades serão facilmente cobertas com este multiprocessador Black+Decker. Descobra uma nova maneira de cozinhar e surpreenda a sua família com as preparações mais ricas. Este aparelho transformará os alimentos no menor tempo possível e se tornará seu ajudante obrigatório na cozinha.",
+        slug: "multiprocessador",
+        precoUnitario: 577,
+        estoque: 27,
+        categoria: "63dd8be23138b0c3320e26fe"
+    },
+    {
+        nome: "Multiprocessador KitchenAid KJA03B 240W empire red 127V",
+        slug: "multiprocessador",
+        precoUnitario: 577,
+        estoque: 27,
+        categoria: "63dd8be23138b0c3320e26fe"
+    },
+    {
+        nome: "Multiprocessador KitchenAid KJA03B 240W empire red 127V",
+        descricao: "Quando se trata de fazer seus pratos favoritos, todas as suas necessidades serão facilmente cobertas com este multiprocessador Black+Decker. Descobra uma nova maneira de cozinhar e surpreenda a sua família com as preparações mais ricas. Este aparelho transformará os alimentos no menor tempo possível e se tornará seu ajudante obrigatório na cozinha.",
+        slug: 1234567890,
+        precoUnitario: 577,
+        estoque: 27,
+        categoria: "63dd8be23138b0c3320e26fe"
+    },
+    {
+        nome: "Multiprocessador KitchenAid KJA03B 240W empire red 127V",
+        descricao: "Quando se trata de fazer seus pratos favoritos, todas as suas necessidades serão facilmente cobertas com este multiprocessador Black+Decker. Descobra uma nova maneira de cozinhar e surpreenda a sua família com as preparações mais ricas. Este aparelho transformará os alimentos no menor tempo possível e se tornará seu ajudante obrigatório na cozinha.",
+        slug: "",
+        precoUnitario: 577,
+        estoque: 27,
+        categoria: "63dd8be23138b0c3320e26fe"
+    },
+    {
+        nome: "Multiprocessador KitchenAid KJA03B 240W empire red 127V",
+        descricao: "Quando se trata de fazer seus pratos favoritos, todas as suas necessidades serão facilmente cobertas com este multiprocessador Black+Decker. Descobra uma nova maneira de cozinhar e surpreenda a sua família com as preparações mais ricas. Este aparelho transformará os alimentos no menor tempo possível e se tornará seu ajudante obrigatório na cozinha.",
+        slug: "mu",
+        precoUnitario: 577,
+        estoque: 27,
+        categoria: "63dd8be23138b0c3320e26fe"
+    },
+    {
+        nome: "Multiprocessador KitchenAid KJA03B 240W empire red 127V",
+        descricao: "Quando se trata de fazer seus pratos favoritos, todas as suas necessidades serão facilmente cobertas com este multiprocessador Black+Decker. Descobra uma nova maneira de cozinhar e surpreenda a sua família com as preparações mais ricas. Este aparelho transformará os alimentos no menor tempo possível e se tornará seu ajudante obrigatório na cozinha.",
+        slug: "m$%ultiprocessador",
+        precoUnitario: 577,
+        estoque: 27,
+        categoria: "63dd8be23138b0c3320e26fe"
+    },
+    {
+        nome: "Multiprocessador KitchenAid KJA03B 240W empire red 127V",
+        descricao: "Quando se trata de fazer seus pratos favoritos, todas as suas necessidades serão facilmente cobertas com este multiprocessador Black+Decker. Descobra uma nova maneira de cozinhar e surpreenda a sua família com as preparações mais ricas. Este aparelho transformará os alimentos no menor tempo possível e se tornará seu ajudante obrigatório na cozinha.",
+        precoUnitario: 577,
+        estoque: 27,
+        categoria: "63dd8be23138b0c3320e26fe"
+    },
 ];
 
 /*
-    *0 - Novo produto
-    *1 - Atualização produto
-    *2 - Código categoria inválido
-    *3 - Código categoria inexistente
-    *4 - Código categoria inativa
-    *5 - Campo nome tipo texto
-    *6 - Campo nome vazio
-    *7 - Campo nome deve ter no mínmo 3 caracteres
-    *8 - Campo nome deve iniciar por uma letra maiúscula
-    *9 - Campo nome é obrigatório
+    *00 - Novo produto
+    *01 - Atualização produto
+    *02 - Código categoria inválido
+    *03 - Código categoria inexistente
+    *04 - Código categoria inativa
+    *05 - Campo nome tipo texto
+    *06 - Campo nome vazio
+    *07 - Campo nome deve ter no mínmo 3 caracteres
+    *08 - Campo nome deve iniciar por uma letra maiúscula
+    *09 - Campo nome é obrigatório
+    *10 - Campo Descrição tipo texto
+    *11 - Campo Descrição vazio
+    *12 - Campo Descrição deve ter no mínmo 3 caracteres
+    *13 - Campo Descrição deve iniciar por uma letra maiúscula
+    *14 - Campo Descrição é obrigatório
+    *15 - Campo Slug tipo texto
+    *16 - Campo Slug vazio
+    *17 - Campo Slug deve ter no mínmo 3 caracteres
+    *18 - Campo Slug deve possuir apenas letras ou números
+    *19 - Campo Slug é obrigatório
 */
 export default DATATEST;
