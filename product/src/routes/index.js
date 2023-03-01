86ab9e73a30135afabba8e1b6deb7f2e8d7fec18
= <paulo.leite@pagonxt.com>
2023-03-01 13:21:42 -0300

    fix: remoção de rota usada apenas no início da API

--- product/src/routes/index.js
+++ product/src/routes/index.js
@@ -2,12 +2,12 @@ import express from 'express';
 import categoriesRoutes from './categoriesRoutes.js';
 import productsRoutes from './productsRoutes.js';
 
-const API = 'API Products e Categories';
+// const API = 'API Products e Categories';
 
 const routes = (app) => {
-    app.route('/').get((_req, res) => {
-        res.status(200).send({ titulo: API });
-    });
+    // app.route('/').get((_req, res) => {
+    //     res.status(200).send({ titulo: API });
+    // });
 
     app.use(
         express.json(),
