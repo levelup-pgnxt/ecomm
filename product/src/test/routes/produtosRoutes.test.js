337d48aa32131d1e8fe6a9565ea3c1bed120fc24
Matheus Leme <matheuslemelhp@gmail.com>
2023-03-01 11:10:48 -0300

    Adicionando teste nos metodos POST e DELETE para a API de produtos

--- product/src/test/routes/produtosRoutes.test.js
+++ product/src/test/routes/produtosRoutes.test.js
@@ -1,5 +1,6 @@
 import app from '../../app.js';
 import request from 'supertest';
+import { response } from 'express';
 
 describe('GET em /api/produtos', () => {
     it('Deve retornar uma lista de produtos', async () => {
@@ -11,3 +12,34 @@ describe('GET em /api/produtos', () => {
     })
 });
 
+let idResposta;
+
+describe('POST em /api/admin/produtos', () => {
+    it('Deve adicionar um novo produto', async () => {
+        const resposta = await request(app)
+            .post('/api/admin/produtos')
+            .send({
+                nome: "Novo Produto de Teste",
+                descricao: "Produto de Teste da Semana de Teste",
+                slug: "produto-teste",
+                precoUnitario: 999.99,
+                quantidadeEmEstoque: 1,
+                categoria: {
+                    nome: "INFORMÁTICA",
+                    idCategoria: "63e0e4d4dd85b8c7425f926f"
+                }
+            })
+            .expect(201)
+
+        idResposta = resposta.body._id;
+    });
+});
+
+
+describe('DELETE em /api/admin/produtos/id', () => {
+    it('Deletar o produto adicionado', async () => {
+        await request(app)
+            .delete(`/api/admin/produtos/${idResposta}`)
+            .expect(200)
+    })
+})
