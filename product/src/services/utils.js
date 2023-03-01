b777f9d79fbb24193cbc7302649dd8afed498c9e
= <paulo.leite@pagonxt.com>
2023-03-01 09:50:29 -0300

    feat: criação da propriedade paramsUpdateProduct onde todos os campos não são obrigatórios.

--- product/src/services/utils.js
+++ product/src/services/utils.js
@@ -60,6 +60,43 @@ const validates = {
         }),
     })),
 
+    paramsUpadateProduct: runSchema(Joi.object().keys({
+        nome: Joi.string().min(limit).empty().pattern(/^[A-Z].+/).messages({
+            'string.base': 'O campo "nome" deve ser do tipo texto!',
+            'string.empty': 'O campo "nome" não deve ser vazio!',
+            'string.min': `O campo "nome" deve ter no mínimo ${limit} caracteres!`,
+            'string.pattern.base': 'O campo "nome" deve iniciar por uma letra maiúscula!',
+        }),
+        descricao: Joi.string().min(limit).empty().pattern(/^[A-Z].+/).messages({
+            'string.base': 'O campo "descrição" deve ser do tipo texto!',
+            'string.empty': 'O campo "descrição" não deve ser vazio!',
+            'string.min': `O campo "descrição" deve ter no mínimo ${limit} caracteres!`,
+            'string.pattern.base': 'O campo "descrição" deve iniciar por uma letra maiúscula!',
+        }),
+        slug: Joi.string().min(limit).empty().pattern(/^[A-Za-z0-9-]+$/).messages({
+            'string.base': 'O campo "slug" deve ser do tipo texto!',
+            'string.empty': 'O campo "slug" não deve ser vazio!',
+            'string.min': `O campo "slug" deve ter no mínimo ${limit} caracteres!`,
+            'string.pattern.base': 'O campo "slug" deve conter apenas letras(maiúsculas ou minúsculas), números ou hífens!',
+        }),
+        precoUnitario: Joi.number().precision(2).greater(0).messages({
+            'number.base': 'O campo "preço unitário" deve ser do tipo numérico!',
+            'number.greater': 'O campo "preço unitário" deve ser maior que zero!',
+        }),
+        estoque: Joi.number().greater(0).less(10000).messages({
+            'number.base': 'O campo "estoque" deve ser do tipo numérico!',
+            'number.greater': 'O campo "estoque" deve ser maior que zero!',
+            'number.less': 'O campo "estoque" deve ser menor que 10.000!',
+        }),
+        desconto: Joi.number().precision(2).messages({
+            'number.base': 'O campo "desconto" deve ser do tipo numérico!',
+        }),
+        categoria: Joi.string().empty().messages({
+            'string.base': 'O campo "categoria" deve ser do tipo texto!',
+            'string.empty': 'O campo "categoria" não deve ser vazio!',
+        }),
+    })),
+
     valuesMaxMin: runSchema(Joi.object().keys({
         max: Joi.number().precision(2).greater(0).required().messages({
             'number.base': 'O campo "máximo" deve ser do tipo numérico!',
