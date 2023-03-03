239a3969f76dda5a0fbe1032ddb20ceec450004d
Matheus Leme <matheuslemelhp@gmail.com>
2023-03-03 14:33:47 -0300

    Adicionando testes para endpoints do subprojeto Order

--- order/server.js
+++ order/server.js
@@ -1,6 +1,13 @@
+/* eslint-disable no-console */
 import app from './src/app.js';
+// import db from './src/config/dbConnect.js';
+
+// db.on('error', console.log.bind(console, 'Erro de conexão com o banco.'));
+// db.once('open', () => console.log('Conexão com o banco realizada com sucesso.'));
+
+// utilizando a conexão do banco pelo server (e não pelo app) e abrindo uma conexão com um banco de
+// testes no arquivo de teste o método patch não funcionou por conta do fetchAPI
 
 const port = process.env.port || 3004;
 
-// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Servidor escutando em http://localhost:${port}`));
