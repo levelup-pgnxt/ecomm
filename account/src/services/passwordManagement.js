506c0a29a73a5f4b33958dcfdfb2d1e736c95cb4
= <paulo.leite@pagonxt.com>
2023-03-08 13:19:12 -0300

    feat: criação das funções createHashWithSalt para conversão da senha do usuário em uma hash com uma pitada de sal, e da função verifyPassword para comparar se a senha passada ao tentar logar é verdadeira.

--- account/src/services/passwordManagement.js
+++ account/src/services/passwordManagement.js
@@ -1,13 +1,15 @@
+/* eslint-disable import/extensions */
 import bcrypt from 'bcryptjs';
+// import NotFoundError from '../errors/NotFoundError.js';
 
-const createHashWihtSalt = (pass) => {
-  const salt = bcrypt.genSaltSync(10);
-  const passWhitHash = bcrypt.hashSync(pass, salt);
+const createHashWihtSalt = async (pass) => {
+  const salt = await bcrypt.genSalt(10);
+  const passWhitHash = await bcrypt.hash(pass, salt);
   return passWhitHash;
 };
 
-const verifyPassword = (pass, passHash) => {
-  const result = bcrypt.compareSync(pass, passHash);
+const verifyPassword = async (pass, passHash) => {
+  const result = await bcrypt.compare(pass, passHash);
   return result;
 };
 
