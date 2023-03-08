36bea350401602ef9dc60e5f8161610f14f00fb8
= <paulo.leite@pagonxt.com>
2023-03-08 13:12:50 -0300

    fix: coreção do nome da API, para Account e exclusão das linhas de comentários

--- account/src/config/dbConnect.js
+++ account/src/config/dbConnect.js
@@ -1,15 +1,12 @@
 import mongoose from 'mongoose';
 
-const HOST = process.env.MONGO_PRODUCT_HOST || '127.0.0.1';
-const USER = process.env.MONGO_PRODUCT_USER || 'admin';
-const PASSWORD = process.env.MONGO_PRODUCT_PASSWORD || 'secret';
-const DATABASE = process.env.MONGO_PRODUCT_DATABASE || 'ecomm-account';
-const PORT = process.env.MONGO_PRODUCT_PORT || '27017';
+const HOST = process.env.MONGO_ACCOUNT_HOST || '127.0.0.1';
+const USER = process.env.MONGO_ACCOUNT_USER || 'admin';
+const PASSWORD = process.env.MONGO_ACCOUNT_PASSWORD || 'secret';
+const DATABASE = process.env.MONGO_ACCOUNT_DATABASE || 'ecomm-account';
+const PORT = process.env.MONGO_ACCOUNT_PORT || '27017';
 
 mongoose.connect(`mongodb://${USER}:${PASSWORD}@${HOST}:${PORT}/${DATABASE}?authSource=admin`);
-
-// mongoose.connect('mongodb://admin:secret@127.0.0.1:27017/ecomm-account?authSource=admin');
-// mongoose.connect('mongodb://admin:secret@ecomm-mongodb:27017/ecomm-account?authSource=admin');
 
 const db = mongoose.connection;
 
