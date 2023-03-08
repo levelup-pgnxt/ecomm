a699c29b3076dc4388126d71f47f8ac86f51e71a
= <paulo.leite@pagonxt.com>
2023-03-08 15:33:20 -0300

    fix: auteração do local de importação da função authenticationStrategyLocal

--- account/src/app.js
+++ account/src/app.js
@@ -5,7 +5,7 @@ import passport from 'passport';
 import db from './config/dbConnect.js';
 import routes from './routes/index.js';
 import errorHandlerMiddleware from './services/erroHandlerMiddleware.js';
-import authenticationStrategyLocal from './services/authenticationStrategy.js';
+import authenticationStrategyLocal from './authentication/authenticationStrategy.js';
 
 db.on('error', console.log.bind(console, 'Erro de conexão!'));
 
