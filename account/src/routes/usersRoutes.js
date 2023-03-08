7e1a27df566f2f522a069c8f82de463e9fbdb24d
= <paulo.leite@pagonxt.com>
2023-03-08 13:16:04 -0300

    feat: criação da rota login

--- account/src/routes/usersRoutes.js
+++ account/src/routes/usersRoutes.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/extensions */
 import express from 'express';
+import passport from 'passport';
 import usersController from '../controllers/usersController.js';
 
 const router = express.Router();
@@ -9,6 +10,7 @@ const pathId = `${path}/:id`;
 const pathAdmin = `/admin${path}`;
 const pathAdminId = `/admin${path}/:id`;
 const pathAdminSearch = `/admin${path}/search`;
+const login = `${pathAdmin}/login`;
 
 router
   .get(pathAdmin, usersController.getAllUsers)
@@ -16,6 +18,7 @@ router
   .get(pathId, usersController.getUserById)
   .post(pathAdmin, usersController.createUser)
   .put(pathAdminId, usersController.updateUser)
-  .delete(pathAdminId, usersController.deleteUserById);
+  .delete(pathAdminId, usersController.deleteUserById)
+  .post(login, passport.authenticate('local', { session: false }), usersController.login);
 
 export default router;
