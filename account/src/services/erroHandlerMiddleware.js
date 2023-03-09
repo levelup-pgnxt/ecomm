ac7837377f3411b5af1c006bf8bb4bbc4c2e9852
= <paulo.leite@pagonxt.com>
2023-03-09 11:50:32 -0300

    feat: inclusão do JsonWebTokenError

--- account/src/services/erroHandlerMiddleware.js
+++ account/src/services/erroHandlerMiddleware.js
@@ -11,6 +11,7 @@ const errorHandlerMiddleware = (err, _req, res, _next) => {
 
   switch (name) {
     case 'ValidationError': res.status(400).json({ message }); break;
+    case 'JsonWebTokenError': res.status(401).json({ message }); break;
     case 'NotFoundError': res.status(code).json({ message }); break;
     default: console.warn(err); res.sendStatus(500);
   }
