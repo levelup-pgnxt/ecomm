accc6b1f36f18193c052fb474d995bf2213c4907
= <paulo.leite@pagonxt.com>
2023-03-09 17:26:49 -0300

    docs: akteração do dado

--- finance/tests/datatests/datatests.js
+++ finance/tests/datatests/datatests.js
@@ -1,10 +1,10 @@
 const DATATEST = [
   {
     value: 2916.43,
-    name: 'VISA',
-    number: '4924018644250794',
-    expiration: '08/2028',
-    cvv: '258',
+    name: 'MASTERCARD',
+    number: '5551511007825100',
+    expiration: '04/2025',
+    cvv: '757',
   },
 ];
 
