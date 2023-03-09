30861341a0a9cd9e56ec3f3ec5020a442fb114af
= <paulo.leite@pagonxt.com>
2023-03-09 13:20:00 -0300

    feat: ajustes com as variáveis do .env

--- finance/src/config/config.js
+++ finance/src/config/config.js
@@ -1,10 +1,8 @@
-require('dotenv').config();
-
 const config = {
-  username: 'root',
-  password: 'secret',
-  database: 'ecomm-finance',
-  host: '127.0.0.1',
+  username: process.env.MYSQL_USER || 'admin',
+  password: process.env.MYSQL_PASSWORD || 'secret',
+  database: process.env.MYSQL_DATABASE || 'ecomm-finance',
+  host: process.env.MYSQL_HOST || '127.0.0.1',
   dialect: 'mysql',
   operatorAliases: false,
 };
