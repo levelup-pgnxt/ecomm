832758db330d6a6c4aa2985502863a204f1e8441
= <paulo.leite@pagonxt.com>
2023-03-10 12:02:03 -0300

    feat: implementado o passport para autenticação e autorização do usuário

--- finance/src/app.js
+++ finance/src/app.js
@@ -1,12 +1,14 @@
-require('dotenv').config();
 const express = require('express');
+const passport = require('passport');
 require('express-async-errors');
 const routes = require('./routes');
 const errorHandlerMiddleware = require('../src/auxiliaries/erroHandlerMiddleware');
+const authenticationStrategyBearer = require('../src/authentication/authentication');
 
 const app = express();
 
 app.use(express.json());
+passport.use(authenticationStrategyBearer);
 
 routes(app);
 
