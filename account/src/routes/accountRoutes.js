3df81bbb8f06866fd373a1b9c339ab3ee63cb6e1
Matheus Leme <matheuslemelhp@gmail.com>
2023-03-10 11:58:09 -0300

    Ajustando rotas da API de accounts

--- account/src/routes/accountRoutes.js
+++ account/src/routes/accountRoutes.js
@@ -4,13 +4,15 @@ import passport from 'passport';
 import AccountController from '../controller/accountController.js';
 
 const router = express.Router();
+const bearerAuthenticate = passport.authenticate('bearer', { session: false });
+const localAuthenticate = passport.authenticate('local', { session: false });
 
 router
-  .get('/api/admin/accounts', passport.authenticate('bearer', { session: false }), AccountController.listarAccounts)
+  .get('/api/admin/accounts', bearerAuthenticate, AccountController.listarAccounts)
   .get('/api/admin/accounts/:id', AccountController.listarAccountPorId)
   .post('/api/admin/accounts', AccountController.criarAccount)
-  .post('/api/accounts/login', passport.authenticate('local', { session: false }), AccountController.login)
-  .put('/api/admin/accounts/:id', passport.authenticate('bearer', { session: false }), AccountController.atualizarAccount)
-  .delete('/api/admin/accounts/:id', passport.authenticate('bearer', { session: false }), AccountController.excluirAccount);
+  .post('/api/accounts/login', localAuthenticate, AccountController.login)
+  .put('/api/admin/accounts/:id', bearerAuthenticate, AccountController.atualizarAccount)
+  .delete('/api/admin/accounts/:id', bearerAuthenticate, AccountController.excluirAccount);
 
 export default router;
