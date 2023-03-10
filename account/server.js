03adb06b6d6e8b66ed926eb5f3028527620ba67e
= <paulo.leite@pagonxt.com>
2023-03-10 11:54:20 -0300

    refactor: remoção da opção 3001

--- account/server.js
+++ account/server.js
@@ -2,7 +2,7 @@
 import {} from 'dotenv/config';
 import app from './src/app.js';
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Servidor escutando em http://localhost:${port}`);
