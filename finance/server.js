17e90a74432cf7a42ff7958768238b8e097301d8
= <paulo.leite@pagonxt.com>
2023-03-10 12:01:05 -0300

    refactor: donenv é requerido no server e a constant PORT passa a receber seu valor apenas do arquivo dotenv

--- finance/server.js
+++ finance/server.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const app = require('./src/app');
 
-const PORT = 3002;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(
   `Servidor está rodando em http://localhost:${PORT}`,
