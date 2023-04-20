1a340a9d0aa94ec90eb89e99acb08542a27a461f
= <paulo.leite@pagonxt.com>
2023-04-20 14:43:55 -0300

    ajuste mudança localização arquivo errorHandlerMiddleware

--- product/src/app.js
+++ product/src/app.js
@@ -4,7 +4,7 @@ import express from 'express';
 import passport from 'passport';
 import db from './config/dbConnect.js';
 import routes from './routes/index.js';
-import errorHandlerMiddleware from './services/erroHandlerMiddleware.js';
+import errorHandlerMiddleware from './auxiliaries/erroHandlerMiddleware.js';
 import authenticationStrategyBearer from './authentication/authentication.js';
 
 db.on('error', console.log.bind(console, 'Erro de conexão!'));
