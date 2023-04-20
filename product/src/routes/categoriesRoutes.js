329a8859fce0ff4c34f314170532adb38c103287
= <paulo.leite@pagonxt.com>
2023-04-20 14:48:45 -0300

    refactor

--- product/src/routes/categoriesRoutes.js
+++ product/src/routes/categoriesRoutes.js
@@ -6,19 +6,13 @@ import categoriesController from '../controllers/categoriesController.js';
 const router = express.Router();
 const passportToken = passport.authenticate('bearer', { session: false });
 
-const path = '/categories';
-const pathId = `${path}/:id`;
-const pathAdminId = `/admin${path}/:id`;
-const pathAdmin = `/admin${path}`;
-const pathSearch = `${path}/search`;
-
 router
-  .get(path, categoriesController.getAllCategories)
-  .get(pathSearch, categoriesController.getCategoryByName)
-  .get(pathId, categoriesController.getCategoryById)
-  .post(pathAdmin, passportToken, categoriesController.createCategory)
-  .put(pathAdminId, passportToken, categoriesController.updateCategory)
-  .patch(pathAdminId, passportToken, categoriesController.changeStatusCategory)
-  .delete(pathAdminId, passportToken, categoriesController.deleteCategoryById);
+  .get('/categories', categoriesController.getAllCategories)
+  .get('/categories/search', categoriesController.getCategoryByName)
+  .get('/categories/:id', categoriesController.getCategoryById)
+  .post('/admin/categories', passportToken, categoriesController.createCategory)
+  .put('/admin/categories/:id', passportToken, categoriesController.updateCategory)
+  .patch('/admin/categories/:id', passportToken, categoriesController.changeStatusCategory)
+  .delete('/admin/categories/:id', passportToken, categoriesController.deleteCategoryById);
 
 export default router;
