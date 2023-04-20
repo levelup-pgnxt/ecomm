4e6175d9a7529c88f2b575d50904f24c52730715
= <paulo.leite@pagonxt.com>
2023-04-20 14:49:30 -0300

    refactor

--- product/src/routes/productsRoutes.js
+++ product/src/routes/productsRoutes.js
@@ -6,34 +6,15 @@ import productsController from '../controllers/productsController.js';
 const router = express.Router();
 const passportToken = passport.authenticate('bearer', { session: false });
 
-const path = '/products';
-const pathSearch = `${path}/search`;
-const pathSearchByValue = `${pathSearch}-by-value`;
-const pathSearchByStock = `${pathSearch}-by-stock`;
-const pathSearchByCategoryId = `${pathSearch}-by-category/:id`;
-const pathId = '/products/:id';
-const pathAdminProducts = `/admin${path}`;
-const pathAdminProductsByID = `/admin${pathId}`;
-
 router
-  .get(path, productsController.getAllProducts)
-  .get(pathSearch, productsController.getProductByName)
-  .get(pathSearchByValue, productsController.getProductsByValue)
-  .get(pathSearchByStock, productsController.getProductsByStock)
-  .get(pathSearchByCategoryId, productsController.getProductsByCategoryId)
-  .get(pathId, productsController.getProductById)
-  .post(pathAdminProducts, passportToken, productsController.createProduct)
-  .put(pathAdminProductsByID, passportToken, productsController.updateProduct)
-  .delete(pathAdminProductsByID, passportToken, productsController.deleteProductById);
+  .get('/products', productsController.getAllProducts)
+  .get('/products/search', productsController.getProductByName)
+  .get('/products/search-by-value', productsController.getProductsByValue)
+  .get('/products/search-by-stock', productsController.getProductsByStock)
+  .get('/products/search-by-category/:id', productsController.getProductsByCategoryId)
+  .get('/products/:id', productsController.getProductById)
+  .post('/admin/products', passportToken, productsController.createProduct)
+  .put('/admin/products/:id', passportToken, productsController.updateProduct)
+  .delete('/admin/products/:id', passportToken, productsController.deleteProductById);
 
 export default router;
-
-// ok .get(path, productsController.getAllProducts)
-// ok .get(pathSearch, productsController.getProductByName)
-// ok .get(pathSearchByValue, productsController.getProductsByValue)
-// ok .get(pathSearchByStock, productsController.getProductsByStock)
-// em andamento .get(pathSearchByCategoryId, productsController.getProductsByCategoryId)
-// ok .get(pathId, productsController.getProductById)
-// ok .post(pathAdminProducts, productsController.createProduct)
-// ok .put(pathAdminProductsByID, productsController.updateProduct)
-// ok .delete(pathAdminProductsByID, productsController.deleteProductById)
