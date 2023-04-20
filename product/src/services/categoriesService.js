19290479466e4b733d16e0ab177a8936a084a7bf
= <paulo.leite@pagonxt.com>
2023-04-20 14:49:55 -0300

    refactor

--- product/src/services/categoriesService.js
+++ product/src/services/categoriesService.js
@@ -1,56 +1,56 @@
 /* eslint-disable import/extensions */
 /* eslint-disable new-cap */
-import categories from '../models/Category.js';
+import Categories from '../models/Category.js';
 
 class CategoryService {
   static getAllCategories = async () => {
-    const result = await categories.find();
+    const result = await Categories.find();
     return result;
   };
 
   static getCategoryById = async (id) => {
-    const category = await categories.findById(id);
+    const category = await Categories.findById(id);
     return category;
   };
 
   static getCategoryByName = async (name) => {
-    const category = await categories.find({ nome: { $regex: name } });
+    const category = await Categories.find({ nome: { $regex: name } });
     return category;
   };
 
   static createCategory = async (data) => {
-    const newCategory = new categories(data);
+    const newCategory = new Categories(data);
     await newCategory.save();
     return newCategory;
   };
 
   static updateCategory = async (id, nome) => {
-    const result = await categories.findByIdAndUpdate(id, { $set: { nome } });
+    const result = await Categories.findByIdAndUpdate(id, { $set: { nome } });
     return result;
   };
 
   static changeStatusCategory = async (id, newStatus) => {
-    const result = await categories.findByIdAndUpdate(id, { $set: { status: newStatus } });
+    const result = await Categories.findByIdAndUpdate(id, { $set: { status: newStatus } });
     return result;
   };
 
   static deleteCategoryById = async (id) => {
-    const result = await categories.findByIdAndDelete(id);
+    const result = await Categories.findByIdAndDelete(id);
     return result;
   };
 
   static checkIsExistsCategory = async (name) => {
-    const exist = await categories.findOne({ nome: name });
+    const exist = await Categories.findOne({ nome: name });
     return !!exist;
   };
 
   static checkIsExistsCategoryById = async (id) => {
-    const exist = await categories.findById(id);
+    const exist = await Categories.findById(id);
     return !!exist;
   };
 
   static checkIsCategoryActive = async (id) => {
-    const exist = await categories.findOne({
+    const exist = await Categories.findOne({
       _id: { $eq: id },
       status: { $eq: 'ATIVA' },
     });
